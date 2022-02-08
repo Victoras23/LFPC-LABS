a993c20525d7705817f82a88b71a24ee6dad9cf1
Victoras23 <87285293+Victoras23@users.noreply.github.com>
2022-02-08 10:40:57 +0200

    Update lab_1_lfpc.py

--- lab 1 lfpc/lab_1_lfpc.py
+++ lab 1 lfpc/lab_1_lfpc.py
@@ -1,14 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-print('after imput you will get :')
-print('1) matrix of ways ')
-print('2) the table with edges and vertex')
-print('3) the first string ')
-print('4) its derivation table ')
-print('5) its derivation tree ')
-print('!!! in order to see te next string , just close the tab with plot !!!')
-
 print('the input should be like :')
 print('enter the Vn : VN={S, A, B, C}, \nenter the Vt : VT={a, b}, \nenter the value of P :P={ \n1. S  bA \n2. A  b \n3. A aB \n4. B  bC \n5. C  cA \n6. A  bA \n7. B  aB }')
 print('\nYou can get the inputs from the txt file in this repository \n')
@@ -27,16 +19,12 @@ points=int(0)
 a=Vn.index('{')
 b=Vn.index('}')
 mx=[]
+
 for i in range(a,b):
     if Vn[i].isalpha(): 
         points+=1
         mx.append(Vn[i])
 
-matrix = [[0 for x in range(points+1)] for y in range(points+1)] 
-
-for i in range(1,points+1):
-    matrix[i][0]=mx[i-1]
-    matrix[0][i]=mx[i-1]
 
 x=len(P)
 edge1=[]
@@ -50,7 +38,6 @@ for i in range(1,(x)):
     for j in range (0,len(P[i])):
         if P[i][j].islower() :
             lg.append(P[i][j])
-            type=False
         elif P[i][j].isupper() :
             if bool:
                 edge1.append(P[i][j])
@@ -62,29 +49,10 @@ for i in range(1,(x)):
     if empty :
        edge2.append(str(ind))
        ind+=1
-
-for i in range(0 , len(edge2)):
-    el1=0
-    el2=0
-    if edge2[i].isdigit()==False: 
-        for j in range(0,len(mx)):
-            if edge2[i]==mx[j]:
-                el1=j
-            if edge1[i]==mx[j]:
-                el2=j
-        matrix[el1+1][el2+1]=lg[i]
-
-print("\n the matrix \n")
-for i in range (points+1):
-    print(matrix[i])
 print("\n the edges \n")
 print(edge1)
 print(edge2)
 print(lg)
-if type :
-    print("\n Type 2 \n")
-else : 
-    print("\n Type 3 \n")
 
 
 string=''
@@ -113,20 +81,69 @@ for i in range (0, len(mx)+7):
         print("string ",nr+0," : " , string)
         print("derivation table",way)
         nr+=1
-        nx.draw(gr, labels=labeldict, with_labels = True)
-        plt.show()
+        #nx.draw(gr, labels=labeldict, with_labels = True)
+        #plt.show()
 
 grx=nx.Graph()
 grx.add_nodes_from(mx)
 l1={}
+l1[edge1[0]]='q0'
+sq='q'
+di=1
+isfinal=0
 for i in range (0, len(edge1)):
     grx.add_edge(edge1[i],edge2[i],length=lg[i])
     if edge2[i].isdigit():
-        l1[edge2[i]]=''
-    else : l1[edge2[i]]=edge2[i]
+        l1[edge2[i]]=sq+str(di)
+        di+=1
+        isfinal+=1
+    elif (edge2[i] in l1.keys())==False:
+        l1[edge2[i]]=sq+str(di)
+        di+=1
 pos = nx.spring_layout(grx)
 nx.draw_networkx(grx,pos, labels=l1, with_labels = True )
 nx.draw_networkx_edge_labels(grx, pos)
 plt.show()
 
+a=Vt.index('{')
+b=Vt.index('}')
+mt=[]
+points1=0
 
+for i in range(a,b):
+    if Vt[i].isalpha(): 
+        points1+=1
+        mt.append(Vt[i])
+
+matrix = [[0 for x in range(points1+1)] for y in range(points+1)] 
+
+for i in range(1,points+1):
+    matrix[i][0]=mx[i-1]
+
+for i in range(1,points1+1):
+    matrix[0][i]=mt[i-1]
+
+for i in range(0 , len(edge2)):
+    el1=0
+    el2=0
+    for j in range(1,points+1):
+        if edge1[i]==matrix[j][0]:
+            el1=j
+    for j in range (1,points1+1):
+        if lg[i]==matrix[0][j]:
+            el2=j
+    matrix[el1][el2]=edge2[i]
+
+print("\n the matrix \n")
+for i in range (points+1):
+    for j in range (points1+1):
+        if matrix[i][j] in l1.keys():
+            matrix[i][j]=l1[matrix[i][j]]
+        else : matrix[i][j]=str(matrix[i][j])
+for i in range (points+1):
+    print(matrix[i])
+
+if isfinal==0 :
+    print("\n Type 2 \n")
+else : 
+    print("\n Type 3 \n")
